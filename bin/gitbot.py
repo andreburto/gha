a08f9976c08e280a9581db081a1603addb1bea1d
Andy Burton <tuglyraisin@gmail.com>
2024-02-18 22:18:40 -0600

    ?

---
+++ bin/gitbot.py
@@ -0,0 +1,16 @@
+import os
+
+from github import Auth, Github
+
+
+auth = Auth.Token(os.getenv("GITHUB_API_TOKEN"))
+
+# Public Web Github
+g = Github(auth=auth)
+
+repo = g.get_repo("andreburto/gha")
+branch = repo.get_branch(branch="master")
+
+print(dir(branch))
+
+g.close()
