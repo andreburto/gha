d168a736df085e30607fa6b35683878be50182d0
Andy Burton <tuglyraisin@gmail.com>
2024-02-24 17:32:59 -0600

    Test the new workflow, 3.

--- bin/gitbot.py
+++ bin/gitbot.py
@@ -2,16 +2,46 @@ import argparse
 import os
 
 from github import Auth, Github
+from sh import git
+
+__author__ = "Andrew Burton"
+
+# Global Variables
+GITHUB_API_TOKEN_KEY = "GH_API_KEY"
 
 
-auth = Auth.Token(os.getenv("GH_API_KEY"))
+def setup_args() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="gitbot", description="Gitbot for Github",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--api-key", dest="api_key", type=str,
+                        default=os.getenv(GITHUB_API_TOKEN_KEY),
+                        help="The API key to use for working in this repo.")
+    parser.add_argument("--branch", dest="branch", type=str, default=os.getenv("GITHUB_REF"),
+                        help="The branch to work with")
+    parser.add_argument("--repo", dest="repo", type=str, default=os.getenv("GITHUB_REPOSITORY"),
+                        help="The repository to work with")
+    return parser.parse_args()
 
-# Public Web Github
-g = Github(auth=auth)
 
-repo = g.get_repo("andreburto/gha")
-branch = repo.get_branch(branch="master")
+def main() -> None:
+    args = setup_args()
 
-print(dir(branch))
+    print(dir(args))
 
-g.close()
+    auth = Auth.Token(os.getenv())
+
+    # Public Web Github
+    g = Github(auth=auth)
+
+    repo = g.get_repo(args.repo)
+    branch = repo.get_branch(args.branch)
+
+    print(dir(branch))
+
+    g.close()
+
+    git.checkout("master")
+
+
+if __name__ == "__main__":
+    main()
