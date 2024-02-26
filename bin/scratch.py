51e0d23c4f556f6e9a7f68f4eab800122ec7ad5a
Andrew Burton <andreburto@users.noreply.github.com>
2024-02-25 22:00:26 -0600

    Fix bug. (#6)

--- bin/scratch.py
+++ bin/scratch.py
@@ -1,12 +1,18 @@
-import github
-import os
-
 import gitbot
+import github
+import logging
+import os
+import sys
 
 __author__ = "Andrew Burton"
 
 MERGE_INDICATOR = "Merge pull request"
 GITHUB_API_TOKEN = os.getenv("GH_API_KEY")
+
+log_level = logging.DEBUG if str(os.getenv("DEBUG", "false")).lower() == "true" else logging.INFO
+logger = logging.getLogger(__file__)
+logger.setLevel(log_level)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
 def main() -> None:
