4d4108246bf72d2d165874fda65bd35146990e88
Andy Burton <tuglyraisin@gmail.com>
2024-11-12 20:59:27 -0600

    Test psql

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+});
+
+pgclient.connect();
+
+pgclient.query('SELECT 1+1 AS result', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    pgclient.end()
+});
